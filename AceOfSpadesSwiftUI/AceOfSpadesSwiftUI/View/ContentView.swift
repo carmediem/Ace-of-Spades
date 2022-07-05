0788f1df215c72ea35eac5b3e95eecc5b15a96a0
Carmen Chiu <carmenchiu@Carmens-iMac.local>
2022-07-05 15:46:16 -0700

    first commit

---
+++ AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AceOfSpadesSwiftUI
+//
+//  Created by Ivan Ramirez on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+   
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
