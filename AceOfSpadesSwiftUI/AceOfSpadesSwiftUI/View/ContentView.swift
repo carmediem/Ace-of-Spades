2ac583510e225d60f21ca31b28c11c76dd2b65e3
Carmen Chiu <carmenchiu@Carmens-iMac.local>
2022-07-05 21:24:52 -0700

    Updated Ace of Spades card

--- AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
+++ AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-   
-  }
+    var body: some View {
+        ZStack {
+              LinearGradient(
+                colors: [Color("Background1")],startPoint: .topLeading,
+                endPoint: .bottomTrailing
+              )
+              .ignoresSafeArea()
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 300, height: 450)
+                    .foregroundColor(Color("Card"))
+                    .cornerRadius(10)
+                
+                ZStack {
+                    Image("Spade")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 100, alignment: .center)
+    
+                    Image("Spade")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 35)
+                        .offset(x: -110, y: -145 )
+                    
+                    Image("Spade")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 35)
+                        .offset(x: -115, y: -155 )
+                        .rotationEffect(.radians(.pi))
+                    
+                    Text("A")
+                        .font(.system(size: 40, weight: .semibold))
+                        .offset(x: -110, y: -180 )
+                    
+                    Text("A")
+                        .font(.system(size: 40, weight: .semibold))
+                        .offset(x: -115, y: -190)
+                        .rotationEffect(.radians(.pi))
+                }
+            }
+        }
+    }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
